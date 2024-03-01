de6c3140d083d3dd81a847de7c8642dedf6b245a
Abir Haque <abir.haque.usa@gmail.com>
2024-03-01 14:39:26 -0600

    Update head.sh

--- MPI-VM-Cluster-Tutorial/head.sh
+++ MPI-VM-Cluster-Tutorial/head.sh
@@ -27,7 +27,6 @@ sudo nano /etc/exports
 
 mkdir cloud
 exportfs -a
-sudo mount -t nfs head:/home/mpiuser/cloud ~/cloud
 df -h  #Check mounted directories
 
 nano /etc/fstab #So we don't have to manually mount shared directory eeach reboot
